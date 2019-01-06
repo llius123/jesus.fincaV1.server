e9085214f1b2af0521fd7d21e9f129cca682141f
Jesus <llius619@gmail.com>
2019-01-06 13:17:43 +0100

    Cors arreglado

--- src/main/java/fincaV1/server/config/Main.java
+++ src/main/java/fincaV1/server/config/Main.java
@@ -1,20 +1,24 @@
-//package fincaV1.server.config;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-//import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.context.annotation.Configuration;
-//
+package fincaV1.server.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 //@Configuration
 //@ComponentScan
 //@EnableAutoConfiguration
-////@SpringBootApplication(exclude=HibernateJpaAutoConfiguration.class)
-//public class Main {
-//
-//	public static void main(String[] args) {
-//		SpringApplication.run(Main.class, args);
-//	}
-//
-//}
+@SpringBootApplication(exclude=HibernateJpaAutoConfiguration.class)
+public class Main {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+
+}
---
+++ src/main/java/fincaV1/server/config/WebSecurityConfig.java
@@ -0,0 +1,15 @@
+package fincaV1.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebSecurityConfig implements WebMvcConfigurer {
+
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**");
+	}
+}
