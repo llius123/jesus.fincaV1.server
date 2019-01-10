6966e370ad70023ccdd1d100fb3701285ccb16d5
llius <llius619@gmail.com>
2019-01-10 20:25:38 +0100

    Cojer el body funcionando, acabar interceptor

--- src/main/java/fincaV1/server/interceptor/FactoryInterceptor.java
+++ src/main/java/fincaV1/server/interceptor/FactoryInterceptor.java
@@ -1,6 +1,9 @@
 package fincaV1.server.interceptor;
 
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,7 +15,12 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import fincaV1.server.entity.ComunidadBean;
 import fincaV1.server.entity.IncidenciaBean;
@@ -26,32 +34,28 @@ public class FactoryInterceptor extends HandlerInterceptorAdapter {
 
 	@Autowired
 	CheckForeignKey checkForeignKey;
-	@Autowired
-	HttpServletRequest request;
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		
+
 		// Forma de saber si el ususario se quiere logear
-		if (isLogin())
+		if (isLogin(request))
 			return true;
 		// Compruebo si hay sesion en server
-		if (!isLogged())
+		if (!isLogged(request))
 			throw new NotLogginSesionException("No estas logeado");
 
 		// Factoria de control para el body de PUT/POST
 		// System.err.println(request.getServletPath());
-//		if(request.getMethod().contentEquals("POST") || request.getMethod().contentEquals("PUT")) {
-//			
-//		}
-		getBody(o);
-		factoryBody();
-
+		if(request.getMethod().contentEquals("POST") || request.getMethod().contentEquals("PUT")) {
+			String body = request.getReader().lines().collect(Collectors.joining());
+			factoryBody(request, body);
+		}
 		return true;
 	}
 
-	private boolean isLogin() {
+	private boolean isLogin(HttpServletRequest request) {
 		String login = request.getRequestURI().substring(16, 21);
 		if (login.equals("login")) {
 			return true;
@@ -60,7 +64,7 @@ public class FactoryInterceptor extends HandlerInterceptorAdapter {
 		}
 	}
 
-	private boolean isLogged() {
+	private boolean isLogged(HttpServletRequest request) {
 		if (request.getSession().getAttribute("vecino") == null) {
 			return false;
 		} else {
@@ -68,7 +72,7 @@ public class FactoryInterceptor extends HandlerInterceptorAdapter {
 		}
 	}
 
-	private <T> boolean factoryBody() {
+	private <T> boolean factoryBody(HttpServletRequest request, String body) {
 		HashMap<T, Integer> hashMap = new HashMap<T, Integer>();
 		String path = request.getServletPath();
 		String table = "";
@@ -88,17 +92,35 @@ public class FactoryInterceptor extends HandlerInterceptorAdapter {
 			}
 		}
 		if (table.equals("incidencias")) {
-			IncidenciaBean incidenciaBean = (IncidenciaBean) o;
-			System.err.println(incidenciaBean);
+			IncidenciaBean o = stringTopojo(body, IncidenciaBean.class);
+			System.err.println(o.getVecino().getId());
 		}
 		return true;
 	}
-
-	private Object o;
-
-	@RequestMapping
-	public void getBody(@RequestBody Object o) {
-		System.err.println(o.toString());
-		this.o = o;
+	
+	private <T> T stringTopojo(String body, Class<T> type) {
+		ObjectMapper objectMapper = new ObjectMapper();
+		Object o = null;
+		try {
+			o = objectMapper.readValue(body,type);
+		} catch (JsonParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return (T) o;
 	}
+
+//	private Object o;
+//
+//	@RequestMapping
+//	public void getBody(@RequestBody Object o) {
+//		System.err.println(o.toString());
+//		this.o = o;
+//	}
 }
