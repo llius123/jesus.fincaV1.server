824f4928b0261875257600aec560a2ea5870c49a
llius <llius619@gmail.com>
2019-01-11 20:44:29 +0100

    Fallo fecha explicacion:
    Esta aceptado que mande integers porque la transformacion de integer a
    fecha se produce de la siguiente forma: un integer es el numero de
    milisegundos transcurridos desde el 1970 hasta la actualidad.
    Por lo tanto cuando transforma un integer lo transforma bien, asi que
    solo hay ue validar la fecha en el caso en el que venga en formato
    string, cosa que ya esta hecha

--- src/main/java/fincaV1/server/interceptor/FactoryInterceptor.java
+++ src/main/java/fincaV1/server/interceptor/FactoryInterceptor.java
@@ -1,36 +1,26 @@
 package fincaV1.server.interceptor;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.sql.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import fincaV1.server.entity.ComunidadBean;
-import fincaV1.server.entity.IncidenciaBean;
-import fincaV1.server.entity.VecinoBean;
-import fincaV1.server.exception.BadBeanFormat;
-import fincaV1.server.exception.NotExistForeignKey;
 import fincaV1.server.exception.NotLogginSesionException;
 import fincaV1.server.factory.CheckForeignKey;
 
-@Component
+@Repository
 public class FactoryInterceptor extends HandlerInterceptorAdapter {
 
 	@Autowired
@@ -47,11 +37,6 @@ public class FactoryInterceptor extends HandlerInterceptorAdapter {
 		if (!isLogged(request))
 			throw new NotLogginSesionException("No estas logeado");
 
-		// Factoria de control para el body de PUT/POST
-//		if(request.getMethod().contentEquals("POST") || request.getMethod().contentEquals("PUT")) {
-//			String body = request.getReader().lines().collect(Collectors.joining());
-//			factoryBody(request, body);
-//		}
 		return true;
 	}
 
@@ -72,56 +57,4 @@ public class FactoryInterceptor extends HandlerInterceptorAdapter {
 		}
 	}
 
-//	private <T> boolean factoryBody(HttpServletRequest request, String body) {
-//		HashMap<T, Integer> hashMap = new HashMap<T, Integer>();
-//		String path = request.getServletPath();
-//		String table = "";
-//		String auxS = "/";
-//		int count = 0;
-//		//Obtengo la tabla a la que estoy haciendo el request
-//		//http...../incidencias/...
-//		//Estoy recojiendo incidencias en este bucle
-//		for (int i = 0; i < path.length(); i++) {
-//
-//			if (auxS.equals(String.valueOf(path.charAt(i)))) {
-//				if (count == 0) {
-//					count++;
-//				} else {
-//					i = path.length();
-//				}
-//			} else {
-//				table = table + path.charAt(i);
-//			}
-//		}
-//		//Aqui llamo a la factoria de permisos
-//		
-//		//Aqui valido el body a ver si las claves ajenas son correctas
-//		if (table.equals("incidencias")) {
-//			IncidenciaBean o = stringTopojo(body, IncidenciaBean.class);
-//		}
-//		return true;
-//	}
-//	
-//	private <T> T stringTopojo(String body, Class<T> type) {
-//		ObjectMapper objectMapper = new ObjectMapper();
-//		Object o = null;
-//		try {
-//			o = objectMapper.readValue(body,type);
-//		} catch (JsonParseException e) {
-//			throw new BadBeanFormat("Formato del bean incorrecto");
-//		} catch (JsonMappingException e) {
-//			throw new BadBeanFormat("Formato del bean incorrecto");
-//		} catch (IOException e) {
-//			throw new BadBeanFormat("Formato del bean incorrecto");
-//		}
-//		return (T) o;
-//	}
-
-//	private Object o;
-//
-//	@RequestMapping
-//	public void getBody(@RequestBody Object o) {
-//		System.err.println(o.toString());
-//		this.o = o;
-//	}
 }
